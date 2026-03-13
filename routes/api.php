747a742bf6fae1f01c3671902a661739f2984724
amineElgaini <elgainiamine@gmail.com>
2026-03-13 15:46:42 +0000

    update the api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\AdminController;
 use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,7 +12,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', function (Request $request) {
-        return $request->user()->load('roles');
+        return $request->user()->load(['role', 'permissions']);
     });
 
     // Admin Routes
@@ -20,5 +21,14 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('users/{user}/permissions', [AdminController::class, 'updatePermissions']);
         Route::get('roles', [RoleController::class, 'index']);
         Route::get('roles/permissions', [RoleController::class, 'rolesWithPermissions']);
+    });
+
+    // Student Routes
+    Route::prefix('student')->group(function () {
+        Route::get('/available-meals', [StudentController::class, 'availableMeals']);
+        Route::get('/reservations', [StudentController::class, 'reservations']);
+        Route::post('/reservations', [StudentController::class, 'reserve']);
+        Route::delete('/reservations/{reservation}', [StudentController::class, 'removeReservation']);
+        Route::post('/reclamations', [StudentController::class, 'submitReclamation']);
     });
 });
