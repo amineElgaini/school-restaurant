819535ba1b6e0519cddcffc5e6bf12f544c9985d
amineElgaini <elgainiamine@gmail.com>
2026-03-16 14:59:25 +0000

    fix menu meal controller

--- app/Http/Controllers/Api/MenuMealController.php
+++ app/Http/Controllers/Api/MenuMealController.php
@@ -13,16 +13,19 @@ class MenuMealController extends Controller implements HasMiddleware
     public static function middleware(): array
     {
         return [
-            new Middleware('permission:manage_menu'),
+            new Middleware('permission:manage_menu', except: ['index']),
         ];
     }
 
-   public function index(Request $request) // Show meals for a date
+    public function index(Request $request) // Show meals for a date
     {
         $request->validate(['date' => 'required|date']);
 
-        $date = $request->query('date');
-        return MenuMeal::with('meal')->where('reservation_date', $date)->get();
+        $date = $request->input('date');
+
+        return MenuMeal::with('meal')
+            ->whereDate('reservation_date', $date)
+            ->get();
     }
 
     public function store(Request $request) // Add meal to menu
@@ -32,6 +35,18 @@ class MenuMealController extends Controller implements HasMiddleware
             'reservation_date' => 'required|date',
         ]);
 
+        // Check if the meal already exists for this date
+        $exists = MenuMeal::where('meal_id', $request->meal_id)
+            ->where('reservation_date', $request->reservation_date)
+            ->exists();
+
+        if ($exists) {
+            return response()->json([
+                'message' => 'This meal is already in the menu for this date'
+            ], 400);
+        }
+
+        // Create new MenuMeal
         $menuMeal = MenuMeal::create([
             'meal_id' => $request->meal_id,
             'reservation_date' => $request->reservation_date,
