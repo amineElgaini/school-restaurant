6a41353537ed23b6013a4fe442789de13e53d2cf
amineElgaini <elgainiamine@gmail.com>
2026-03-16 15:31:36 +0000

    udpate the routes

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,8 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::apiResource('meals', MealController::class);
     Route::apiResource('menu-meals', MenuMealController::class)->except(['show', 'update']);
-
+    Route::apiResource('reservations', ReservationController::class)->only(['index', 'show']);
+    Route::get('reservations/stats', [ReservationController::class, 'stats']);
 
     // Student Routes
     Route::prefix('student')->group(function () {
